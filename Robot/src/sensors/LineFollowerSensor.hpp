79c54a89b601696ef3612463d65cfc0c6e6340a9
agisboye <august@heegaard.org>
2019-11-18 02:04:28 -0800

    Remove comments

--- Robot/src/sensors/LineFollowerSensor.hpp
+++ Robot/src/sensors/LineFollowerSensor.hpp
@@ -13,21 +13,10 @@ class LineFollowerSensor : public Sensor {
     void reset();
 
     unsigned int *values;
-    // unsigned int value1;
-    // unsigned int value2;
-    // unsigned int value3;
-    // unsigned int value4;
-    // unsigned int value5;
 
   private:
     int *pins;
     const unsigned int sensorCount;
-
-    // const int ldr0Pin;
-    // const int ldr1Pin;
-    // const int ldr2Pin;
-    // const int ldr3Pin;
-    // const int ldr4Pin;
 };
 
 #endif
