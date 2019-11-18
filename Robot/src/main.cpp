1783e7955086556aee78fbbf2b1110066410463a
agisboye <august@heegaard.org>
2019-11-18 02:03:38 -0800

    Implement line follower sensor

--- Robot/src/main.cpp
+++ Robot/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 
-#include "sensors/UltrasonicSensor.hpp"
 #include "sensors/IMUSensor.hpp"
 #include "sensors/IRSensor.hpp"
+#include "sensors/LineFollowerSensor.hpp"
+#include "sensors/UltrasonicSensor.hpp"
 
 // Example of how to create two ultrasonic sensors.
 UltrasonicSensor usFrontLeft(DD1, DD2);
@@ -14,20 +15,22 @@ IMUSensor imu;
 // Example of how to create IR sensor
 IRSensor ir1(13, A0);
 
-void setup() {
-    Serial.begin(9600);
-}
+// Example of how to create line follower sensor
+int linefollowerPins[] = {1, 2, 3, 4, 5};
+LineFollowerSensor lf(linefollowerPins, 5);
+
+void setup() { Serial.begin(9600); }
 
 void loop() {
-    
+
     // Update sensors
     usFrontLeft.update();
     usFrontRight.update();
     imu.update();
     ir1.update();
+    lf.update();
 
     // Update state machine
 
     // Carry out actions
-
 }
--- Robot/src/sensors/LineFollowerSensor.cpp
+++ Robot/src/sensors/LineFollowerSensor.cpp
@@ -1,7 +1,49 @@
 #include "LineFollowerSensor.hpp"
+#include <Arduino.h>
 
-LineFollowerSensor::LineFollowerSensor() {}
+// The minimum number of milliseconds that should pass
+// between each sample.
+#define SAMPLE_FREQUENCY 100
 
-void LineFollowerSensor::update() {}
+LineFollowerSensor::LineFollowerSensor(int *pins, unsigned int sensorCount)
+    : sensorCount(sensorCount) {
+    this->values = new unsigned int[sensorCount];
+    this->pins = new int[sensorCount];
 
-void LineFollowerSensor::reset() {}
+    for (unsigned int i = 0; i < sensorCount; i++) {
+        // Copy each pin and configure pin mode
+        this->pins[i] = pins[i];
+        pinMode(pins[i], INPUT);
+
+        // Initialize value
+        this->values[i] = 0;
+    }
+}
+
+LineFollowerSensor::~LineFollowerSensor() {
+    delete[] values;
+    delete[] pins;
+}
+
+void LineFollowerSensor::update() {
+
+    unsigned long now = millis();
+
+    // Check if we can update the sensor data.
+    if (now - lastUpdate < SAMPLE_FREQUENCY) {
+        return;
+    }
+
+    for (unsigned int i = 0; i < this->sensorCount; i++) {
+        this->values[i] = (unsigned int)analogRead(this->pins[i]);
+    }
+}
+
+void LineFollowerSensor::reset() {
+
+    for (unsigned int i = 0; i < this->sensorCount; i++) {
+        this->values[i] = 0;
+    }
+
+    this->lastUpdate = 0;
+}
