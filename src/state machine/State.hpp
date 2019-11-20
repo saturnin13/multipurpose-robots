ed0d5d40ccd0d706e2d6a12dec60e18bc410d750
agisboye <august@heegaard.org>
2019-11-20 15:59:14 -0800

    Fix compilation errors

--- src/state machine/State.hpp
+++ src/state machine/State.hpp
@@ -10,7 +10,7 @@ public:
 
     enum TableState {UNSEEN, CURRENT, COMPLETED};
     TableState LineFollowingTable = UNSEEN;
-    TableState HillTable = UNSEEN
+    TableState HillTable = UNSEEN;
     TableState FinalTable = UNSEEN;
     bool totalTurnedDegrees = 0;
 };
